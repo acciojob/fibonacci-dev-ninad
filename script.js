b6a7563d1bf12a74d8b612808a0e88943f1929c3
dev-ninad <126334754+dev-ninad@users.noreply.github.com>
2023-05-12 18:37:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
 // your code here
+	var fib = [];
+  fib[0] = 0;
+  fib[1] = 1;
+  for (var i = 2; i <= num; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[num];
 }
 
 module.exports = fibonacci;
